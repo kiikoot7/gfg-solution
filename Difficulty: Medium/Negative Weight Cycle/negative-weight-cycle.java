afbcf9384b8bdab0ea04e2ebfbb9280d1aced907
Anshika Singh <kiikoot7@gmail.com>
2026-09-01 16:04:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Negative Weight Cycle/negative-weight-cycle.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isNegativeWeightCycle(int V, int[][] edges) {
+        long[] dist = new long[V];
+
+        for (int i = 0; i < V - 1; i++) {
+            boolean updated = false;
+            for (int[] edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                long w = edge[2];
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    updated = true;
+                }
+            }
+            if (!updated) {
+                break;
+            }
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            long w = edge[2];
+            if (dist[u] + w < dist[v]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
